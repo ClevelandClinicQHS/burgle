e165f1779ac885c3a5d9404ccbf4bd8f8534d0e4
Paul Gunsalus <gunsalp@ccf.org>
2024-04-01 14:37:20 -0400

    Fixing, null coxph and CSC models

--- R/burgle_survival.R
+++ R/burgle_survival.R
@@ -3,18 +3,35 @@
 #' @export
 burgle.coxph <- function(object, ...){
   bh <- survival::basehaz(object, centered = FALSE)
-  formula <- stats::reformulate(as.character(attr(object$terms, "predvars"))[-c(1:2)])
-  if(!is.null(object$strata)| grepl("strata", names(object$xlevels))){
+  ft <- as.character(attr(object$terms, "predvars"))[-c(1:2)]
+  ## intercept only model
+  if(length(ft)<1){
+    formula <- stats::formula(paste0("~1"))
+  }else{
+    formula <- stats::reformulate(ft)
+  }
+  ## or no other coefficients
+  if(!is.null(object$xlevels) && (!is.null(object$strata)| any(grepl("strata", names(object$xlevels))))){
     bh0 <- bh[, c("hazard", "strata")]
     bh <- bh[!duplicated(bh0),]
     ft <- as.character(attr(object$terms, "predvars"))[-c(1:2)]
-    formula <- stats::reformulate(ft[!grepl("strata", ft)])
-
+    ft2 <- ft[!grepl("strata", ft)]
+    if(length(ft2)<1){
+      formula <- stats::formula(paste0("~1"))
+    }else{
+      formula <- stats::reformulate(ft2)
+    }
   }else{
     bh <- bh[!duplicated(bh$hazard),]
   }
   coef <- stats::coef(object)
-  cov <- stats::vcov(object)
+  ## 0 coefficients
+  if(length(coef) == 0L){
+    cov <- matrix(0)
+    # coef <- 0L
+  }else{
+    cov <- stats::vcov(object)
+  }
   rss <- sum(object$residuals ^2)/(object$n - length(coef))
   xlevels <- object$xlevels
   l <- list("coef" = coef,
@@ -44,13 +61,24 @@ burgle.CauseSpecificCox <- function(object, ...){
   hazards <- lapply(basehzs, function(x) matrix(x[["hazard"]], byrow = FALSE, nrow = Net))
   levT <- lapply(basehzs, `[[`, "lastEventTime")
   coefs <- lapply(models, stats::coef)
-  vcovs <- lapply(models, stats::vcov)
+
+  vcovs <- mapply(function(x, y) if(length(x) == 0L){return(matrix(0))}else{stats::vcov(y)}, coefs, models, SIMPLIFY = FALSE)
+  # coefs <- lapply(coefs, function(x) if(is.null(x)){return(0L)}else{x})
 
   formulas <- lapply(models, function(x){
-    f1 <- stats::reformulate(as.character(attr(x$terms, "predvars"))[-c(1:2)])
+    ft <- as.character(attr(x$terms, "predvars"))[-c(1:2)]
+    # f1 <- stats::reformulate(ft)
+    if(length(ft) <1){
+      return(stats::formula(paste0("~1")))
+    }
+    f1 <- stats::reformulate(ft)
     if(!is.null(x$strata)){
       ft <- as.character(attr(x$terms, "predvars"))[-c(1:2)]
-      f1 <- stats::reformulate(ft[!grepl("strata", ft)])
+      ft2 <- ft[!grepl("strata", ft)]
+      if(length(ft2) <1){
+        return(stats::formula(paste0("~1")))
+      }
+      f1 <- stats::reformulate(ft2)
     }
     f1
   })
@@ -74,6 +102,7 @@ burgle.CauseSpecificCox <- function(object, ...){
 
 }
 
+## need to adapt the predict methods for coxph and CSC if length(coef) == 0
 
 
 
